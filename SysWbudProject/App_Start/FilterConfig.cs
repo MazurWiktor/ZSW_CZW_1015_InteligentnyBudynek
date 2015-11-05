647b1faa20c866bdca624327b24e96e6ffbe3d47
KamilSojka <sojka.kamill@gmail.com>
2015-11-05 11:00:21 +0100

    Pusty projekt gotowy do dzialania

---
+++ SysWbudProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SysWbudProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
