3445d9acfe4114ba5b5dd2b637ec2ef202765e91
mateusz353 <mateusz.nowakowski.pwr@gmail.com>
2015-11-16 17:00:09 +0100

    Komunikacja z serial portem.

---
+++ SysWbudProject/SafeSerialPort/SafeSerialPort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Web;
+
+namespace SmartHome.SerialPorts
+{
+    public class SafeSerialPort : SerialPort
+    {
+        public SafeSerialPort(string s, int p1, Parity parity, int p2)
+            : base(s, p1, parity, p2)
+        {
+
+        }
+        public new void Open()
+        {
+            if (!base.IsOpen)
+            {
+                base.Open();
+                GC.SuppressFinalize(this.BaseStream);
+            }
+        }
+
+        public new void Close()
+        {
+            if (base.IsOpen)
+            {
+                GC.ReRegisterForFinalize(this.BaseStream);
+                base.Close();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                base.Dispose(disposing);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
